e47c6dac249cdb93c93fa50d39916e212ea8c2e4
fox666 <damnfox666@yandex.ru>
2023-11-07 15:31:54 +0300

    Домашка 1-3

--- src/main/kotlin/task1/Task1.kt
+++ src/main/kotlin/task1/Task1.kt
@@ -6,10 +6,12 @@ fun main() {
     println("Привет $name")
     println("Введите число от 1 до 3: ")
     var number = 0
-
-    while (number != 1 && number != 2 && number != 3) {
-        number = readln().toInt()
-        println("Некоретное число, введите еще раз:")
+    number = readln().toInt()
+    if (number != 1 && number != 2 && number != 3) {
+        while (number != 1 && number != 2 && number != 3) {
+            println("Некоретное число, введите еще раз:")
+            number = readln().toInt()
+        }
     }
     println("Введите ставку: ")
     val stavka = readln().toInt()
@@ -17,5 +19,5 @@ fun main() {
 
     if (numb == number)
         println("Вы выиграли " + stavka * 3)
-     else println("Вы проиграли, лох ")
+    else println("Вы проиграли, лох ")
 }
